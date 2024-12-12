f111f2677a37b2464ba29d7421f8f066dd1c0451
zille <azalfamurtaza32@gmail.com>
2024-12-12 10:01:18 +0100

    anado operadores

---
+++ operadores.js
@@ -0,0 +1,16 @@
+/*//comparison operator examples
+
+//== (igualdad)
+
+   console.log(10 == "10"); //true porque solo compara el valor no el tipo
+
+//=== (igualdad estrictamente)
+   console.log(30==="30");// false porque ===compara tanto el valor como el tipo
+
+//!=  (no igual)
+   console.log(5!=10); //true porque 5 es no igual a 10
+
+//!== (no igual estrictamente)
+      
+   console.log(7!=="20");// true porque 5(numero) es no igual estrictamente a 20(cadena)*/
+           
